fcad0bfaa96ac7be4131a987180899ede1a2ce3c
macchiagabriele13 <macchia_gabriele@libero.it>
2023-01-17 16:51:00 +0100

    Commit - Seeding Tech DB

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['Nodejs', 'Bootstrap', 'Vuejs', 'SCSS', 'React', 'Webflow'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($newTechnology->name);
+            $newTechnology->save();
+        }
     }
 }
